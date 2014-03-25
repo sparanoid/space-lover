62a5b7c0975f116b8fcec15d58fc089ae08ca9f8
Tunghsiao Liu <t@sparanoid.com>
2014-03-25 14:21:17 +0800

    Init. commit

---
+++ README.txt
@@ -0,0 +1,71 @@
+=== Space Lover ===
+Contributors: Sparanoid
+Donate link: http://sparanoid.com/donate/
+Tags: comments, content, javascript, chinese, china, copywriting
+Requires at least: 3.5.1
+Tested up to: 3.9
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Magically add an extra space between Chinese / Japanese characters and English letters / numbers / common punctuation marks
+
+== Description ==
+
+This plugin uses [vinta/paranoid-auto-spacing](https://github.com/vinta/paranoid-auto-spacing) to magically add an extra space between Chinese / Japanese characters and English letters / numbers / common punctuation marks.
+
+A must-have plugin if you're running a Chinese / Japanese blog. This plugin also works fine with multisite enabled WordPress (aka. WordPress Mu).
+
+No options, no additional database inserts, no stupid banners and shitty ads, install and go.
+
+神奇的在中文、日文和英文、數字、符號之間自動加入空白
+
+Before:
+
+    今天出去買菜花了5000元
+
+After:
+
+    今天出去買菜花了 5000 元
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+= Using The WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'Space Lover'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `space-lover.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `space-lover.zip`
+2. Extract the `space-lover` directory to your computer
+3. Upload the `space-lover` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+== Frequently Asked Questions ==
+
+= Compatible with multisite? =
+
+Yep.
+
+== Changelog ==
+
+= 1.0.0 =
+* First release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* First release
---
+++ space-lover.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package   Space_Lover
+ * @author    Tunghsiao Liu <t@sparanoid.com>
+ * @license   GPL-2.0+
+ * @link      http://sparanoid.com/
+ * @copyright Sparanoid
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Space Lover
+ * Plugin URI:        https://github.com/sparanoid/space-lover
+ * Description:       Magically add an extra space between Chinese / Japanese characters and English letters / numbers / common punctuation marks
+ * Version:           1.0.0
+ * Author:            Tunghsiao Liu
+ * Author URI:        http://sparanoid.com/
+ * Text Domain:       space-lover
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/sparanoid/space-lover
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-space-lover.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ */
+register_activation_hook( __FILE__, array( 'Space_Lover', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Space_Lover', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'Space_Lover', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * If you want to include Ajax within the dashboard, change the following
+ * conditional to:
+ *
+ * if ( is_admin() ) {
+ *   ...
+ * }
+ *
+ * The code below is intended to to give the lightest footprint possible.
+ */
+// if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+//
+//   require_once( plugin_dir_path( __FILE__ ) . 'admin/class-space-lover-admin.php' );
+//   add_action( 'plugins_loaded', array( 'Space_Lover_Admin', 'get_instance' ) );
+//
+// }
